5457d45dd96b6e65161a01b37b74a5dd27d18304
Ryan Shabaneh <ryan1000tkd@gmail.com>
2026-03-24 16:26:07 -0400

    valid_anagram

--- arrays and hashmaps/contains_duplicate.py
+++ arrays and hashmaps/contains_duplicate.py
@@ -6,11 +6,18 @@
 #otherwise return false.
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) < len(nums) #I got it right!!
+        hashset = set()
+        for num in nums:
+            if num in set:
+                return True
+            hashset.add(n)
+            return False
 
-
+        # return len(set(nums)) < len(nums) #I got it right!! # shortcut
 
 '''
+Time Complexity is O(N) I loop through the nums array once
+Space Complexity is O(N), I need to make a new hashset with size up to N
 extra notes
 If nums is a list: nums = [1, 2, 2, 3, 3, 3]
 Then: set(nums) -> {1, 2, 3}
