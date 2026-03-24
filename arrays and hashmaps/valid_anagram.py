a3903cbc5802a7e1935a088195b1c8643863cb01
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-03-24 16:53:01 -0400

    notes for anagram

--- arrays and hashmaps/valid_anagram.py
+++ arrays and hashmaps/valid_anagram.py
@@ -9,19 +9,26 @@ but the order of the characters can be different.
 '''
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)! = len(t) return False
+        if len(s)! = len(t):
+            return False
         countS = {}
         countT = {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get[s[i], 0]
-            countT[t[i]] = 1 + countT.get[t[i], 0]
+            countS[s[i]] = 1 + countS.get([s[i], 0])
+            countT[t[i]] = 1 + countT.get([t[i], 0])
         return countS == countT
 
 
 '''
 Notes:
+Even if string is length 1,000,000: "aaaaabbbbbccccc..." My dict size is STILL ≤ 26 
+Time complexity: O ( n + m ) 
+Space complexity: O(1) since we have at most  26 different characters.
+
 Remember to check length right away
+Key Insight You can process both strings in one pass
+
 '''
 
 
