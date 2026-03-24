033be329f9bb00efdcd14b986df1721778b3aeed
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-03-24 16:45:11 -0400

    fix right email

--- arrays and hashmaps/valid_anagram.py
+++ arrays and hashmaps/valid_anagram.py
@@ -15,10 +15,7 @@ class Solution:
 
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get[s[i], 0]
-            countT[t[i]] = 1 + countT.get[t[i], 0]
-        return countS == countT
-
-
+           i++amajr0.0ajs
 '''
 Notes:
 Remember to check length right away
