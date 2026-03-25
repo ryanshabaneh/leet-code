6071698cd5828d176c85addcdfffefd3ac6d8e05
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-03-25 09:48:08 -0400

    setup for two sum

--- arrays and hashmaps/two_sum.py
+++ arrays and hashmaps/two_sum.py
@@ -1,6 +1,19 @@
 # 1. Two Sum
 # https://leetcode.com/problems/two-sum/
 # Difficulty: Easy
-#
-# Given an array of integers nums and an integer target,
-# return indices of the two numbers such that they add up to target.
+
+'''
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+
+Example 1:
+Input: 
+
+nums = [3,4,5,6], target = 7
+Output: [0,1]
+Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+'''
