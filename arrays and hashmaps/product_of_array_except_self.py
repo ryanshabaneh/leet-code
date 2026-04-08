0126533c952cb39487862b5692ef7d32fad13cfe
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-07 23:10:19 -0400

    set up

--- arrays and hashmaps/product_of_array_except_self.py
+++ arrays and hashmaps/product_of_array_except_self.py
@@ -24,4 +24,8 @@ Constraints:
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+
