7f864be7bf23d7252402167e8986c6dceee3598b
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-08 16:37:43 -0400

    new problem)

---
+++ arrays and hashmaps/longest_consecutive_sequence.py
@@ -0,0 +1,7 @@
+# 128. Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence/
+# Difficulty: Medium
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        pass
