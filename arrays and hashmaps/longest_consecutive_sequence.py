7f80b1139b42bca9e54336defcb0211aa6436b43
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-08 16:38:03 -0400

    notes

--- arrays and hashmaps/longest_consecutive_sequence.py
+++ arrays and hashmaps/longest_consecutive_sequence.py
@@ -2,6 +2,28 @@
 # https://leetcode.com/problems/longest-consecutive-sequence/
 # Difficulty: Medium
 
+'''
+Given an array of integers nums, return the length of the longest
+consecutive sequence of elements that can be formed.
+
+A consecutive sequence is a sequence where each element is exactly 1
+greater than the previous. Elements do not need to be consecutive in
+the original array.
+
+Must run in O(n) time.
+
+Example 1:
+Input: nums = [2,20,4,10,3,4,5]
+Output: 4
+Explanation: The longest consecutive sequence is [2,3,4,5]
+
+Example 2:
+Input: nums = [0,3,2,5,4,6,1,1]
+Output: 7
+
+
+'''
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         pass
