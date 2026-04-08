ecd1e208b0f29f0ee758fee5c1f647552b66af9f
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-07 20:02:45 -0400

    new lc

--- arrays and hashmaps/product_of_array_except_self.py
+++ arrays and hashmaps/product_of_array_except_self.py
@@ -1,6 +1,27 @@
 # 238. Product of Array Except Self
 # https://leetcode.com/problems/product-of-array-except-self/
 # Difficulty: Medium
-#
-# Given an integer array nums, return an array answer such that answer[i]
-# is equal to the product of all elements of nums except nums[i].
+
+'''
+Given an integer array nums, return an array output where output[i] is the
+product of all elements of nums except nums[i].
+
+Each product is guaranteed to fit in a 32-bit integer.
+Follow-up: solve in O(n) time without using division.
+
+Example 1:
+Input: nums = [1,2,4,6]
+Output: [48,24,12,8]
+
+Example 2:
+Input: nums = [-1,0,1,2,3]
+Output: [0,-6,0,0,0]
+
+Constraints:
+2 <= nums.length <= 1000
+-20 <= nums[i] <= 20
+'''
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pass
