78833c09fc68e71ebdd1f46692714a9410e7b682
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-08 22:14:12 -0400

    loop cond

--- arrays and hashmaps/longest_consecutive_sequence.py
+++ arrays and hashmaps/longest_consecutive_sequence.py
@@ -30,3 +30,6 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
+        for n in nums:
+            if (n - 1) not in numSet:
+
