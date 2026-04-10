6256fe5e7d2897971f8248309dbdfcce36a42185
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-10 15:29:00 -0400

    2P

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -1,6 +1,28 @@
 # 125. Valid Palindrome
 # https://leetcode.com/problems/valid-palindrome/
 # Difficulty: Easy
-#
-# A phrase is a palindrome if, after converting all uppercase letters to lowercase
-# and removing all non-alphanumeric characters, it reads the same forward and backward.
+
+'''
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome reads the same forward and backward.
+It is case-insensitive and ignores all non-alphanumeric characters.
+
+Example 1:
+Input: s = "Was it a car or a cat I saw?"
+Output: true
+Explanation: "wasitacaroracatisaw" is a palindrome.
+
+Example 2:
+Input: s = "tab a cat"
+Output: false
+Explanation: "tabacat" is not a palindrome.
+
+Constraints:
+1 <= s.length <= 1000
+s consists of only printable ASCII characters.
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pass
