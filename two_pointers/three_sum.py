5364f6dabdf13822e7d37db51ccf4b427a5d0b88
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-11 18:07:35 -0400

    notes

--- two_pointers/three_sum.py
+++ two_pointers/three_sum.py
@@ -1,11 +1,23 @@
 # 15. 3Sum
 # Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
-# such that i != j, i != k, j != k, and nums[i] + nums[j] + nums[k] == 0.
-# The solution set must not contain duplicate triplets.
+# where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+# The output should not contain any duplicate triplets.
 #
-# Example:
-# Input: nums = [-1, 0, 1, 2, -1, -4]
-# Output: [[-1, -1, 2], [-1, 0, 1]]
+# Example 1:
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+#
+# Example 2:
+# Input: nums = [0,1,1]
+# Output: []
+#
+# Example 3:
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+#
+# Constraints:
+# - 3 <= nums.length <= 1000
+# - -10^5 <= nums[i] <= 10^5
 
 from typing import List
 
