87eaace218e6e5a2283004e39f86ff7cf743fa91
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-12 15:21:00 -0400

    new file

---
+++ sliding_window/best_time_to_buy_sell_stock.py
@@ -0,0 +1,24 @@
+# 121. Best Time to Buy and Sell Stock
+# You are given an integer array prices where prices[i] is the price on the ith day.
+# You may choose a single day to buy and a different future day to sell.
+# Return the maximum profit you can achieve. Return 0 if no profit is possible.
+#
+# Example 1:
+# Input: prices = [10,1,5,6,7,1]
+# Output: 6
+# Explanation: Buy on day 1 (price=1), sell on day 4 (price=7), profit = 6
+#
+# Example 2:
+# Input: prices = [10,8,7,5,2]
+# Output: 0
+# Explanation: No profitable transactions possible
+#
+# Constraints:
+# - 1 <= prices.length <= 100
+# - 0 <= prices[i] <= 100
+
+from typing import List
+
+
+def max_profit(prices: List[int]) -> int:
+    pass
