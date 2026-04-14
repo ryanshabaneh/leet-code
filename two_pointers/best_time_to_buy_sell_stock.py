07c074a728635a7a6191533f4a776a6cc6b91e2b
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-13 23:11:13 -0400

    fixed r pointer:

--- two_pointers/best_time_to_buy_sell_stock.py
+++ two_pointers/best_time_to_buy_sell_stock.py
@@ -30,7 +30,7 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        l,r = 0, len(prices)-1
+        l,r = 0, 1
 
         while l<r:
             
