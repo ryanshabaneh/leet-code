b31958b277378003bdba940ecda63f39ae515205
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-17 01:30:58 -0400

    maxf optimization

--- sliding_window/longest_repeating_character_replacement.py
+++ sliding_window/longest_repeating_character_replacement.py
@@ -23,18 +23,37 @@
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        result = 0
+
+        maxf = 0
+        res = 0
         l = 0
         count = {}
-        maxFreq = 0
 
         for r in range(len(s)):
+            maxf = max (maxf, count[s[r]])
             count[s[r]] = 1 + count.get(s[r], 0)
-            if (r - l + 1) - max(count.values) > k:
+            if (r - l + 1) - maxf > k:
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
         return res
+    
+
+
+        '''
+        res = 0
+        l = 0
+        count = {}
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            if (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+
+        '''
 
 
         # Correctness guarantee: r visits every position in the string.
