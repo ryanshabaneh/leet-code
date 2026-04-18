1bb15ac8e6119a4985df87b9f21a2574f22ca9e0
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-18 15:00:00 -0400

    stacks

--- stack/valid_parentheses.py
+++ stack/valid_parentheses.py
@@ -2,5 +2,31 @@
 # https://leetcode.com/problems/valid-parentheses/
 # Difficulty: Easy
 #
-# Given a string s containing just '(', ')', '{', '}', '[', ']',
-# determine if the input string is valid.
+# You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+#
+# The input string s is valid if and only if:
+# - Every open bracket is closed by the same type of close bracket.
+# - Open brackets are closed in the correct order.
+# - Every close bracket has a corresponding open bracket of the same type.
+#
+# Return true if s is a valid string, and false otherwise.
+#
+# Example 1:
+# Input: s = "[]"
+# Output: true
+#
+# Example 2:
+# Input: s = "([{}])"
+# Output: true
+#
+# Example 3:
+# Input: s = "[(])"
+# Output: false
+# Explanation: The brackets are not closed in the correct order.
+#
+# Constraints:
+# 1 <= s.length <= 1000
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pass
