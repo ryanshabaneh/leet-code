ce5ab79473e64c8657860d962c947dfa000702a0
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-04-28 18:42:39 -0400

    done

--- linked_list/206_reverse_linked_list.py
+++ linked_list/206_reverse_linked_list.py
@@ -15,6 +15,14 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        prev, curr = None, head
+
+        while curr != None:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        return prev
+        
 
      
