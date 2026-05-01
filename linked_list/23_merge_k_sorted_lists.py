28bc1f9627a5739b9d362d85d05d1b7a83f92d2e
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-05-01 15:11:08 -0400

    new problem

---
+++ linked_list/23_merge_k_sorted_lists.py
@@ -0,0 +1,39 @@
+# 23. Merge K Sorted Linked Lists
+# https://leetcode.com/problems/merge-k-sorted-lists/
+# Difficulty: Hard
+#
+# You are given an array of k linked lists lists, where each list is sorted
+# in ascending order.
+#
+# Return the sorted linked list that is the result of merging all of the
+# individual linked lists.
+#
+# Example 1:
+#   Input:  lists = [[1, 2, 4], [1, 3, 5], [3, 6]]
+#   Output: [1, 1, 2, 3, 3, 4, 5, 6]
+#
+# Example 2:
+#   Input:  lists = []
+#   Output: []
+#
+# Example 3:
+#   Input:  lists = [[]]
+#   Output: []
+#
+# Constraints:
+#   0 <= lists.length <= 1000
+#   0 <= lists[i].length <= 100
+#   -1000 <= lists[i][j] <= 1000
+
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        pass
