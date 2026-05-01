6ebe4579e7b2b618a5803189c3f81ece814c4b1f
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-05-01 14:08:31 -0400

    start left at window to offset it by n+1

--- linked_list/19_remove_nth_node_from_end.py
+++ linked_list/19_remove_nth_node_from_end.py
@@ -37,7 +37,7 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0, head) 
-        left, right = head, head
+        left, right = dummy, head
 
         for _ in range(n):
             right = right.next
