34d5777df3f30d74ba76324de19e55752b5aa1b5
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-05-03 23:57:04 -0400

    recursion work

--- trees/104_maximum_depth_of_binary_tree.py
+++ trees/104_maximum_depth_of_binary_tree.py
@@ -32,5 +32,4 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        else:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
