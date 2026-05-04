d53c3d05d12f74aceb07371dad030f92faff7933
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-05-04 19:43:32 -0400

    redo fresh slate

--- trees/111_minimum_depth_of_binary_tree.py
+++ trees/111_minimum_depth_of_binary_tree.py
@@ -32,12 +32,5 @@ class TreeNode:
 
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        if not root.left:
-            return 1 + self.minDepth(root.right)
-        if not root.right:
-            return 1 + self.minDepth(root.left)
-        pass 
+        pass
+    
