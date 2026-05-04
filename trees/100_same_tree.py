8a8d325599592d7d9ee3055a4b3a95af628431d6
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-05-04 00:53:52 -0400

    recursion work

--- trees/100_same_tree.py
+++ trees/100_same_tree.py
@@ -44,5 +44,4 @@ class Solution:
             return False
         if p.val != q.val:
             return False
-        isSameTree(p.left, q.left)
-        isSameTree(p.right, q.right)
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
