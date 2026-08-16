4f50c3eca5d430f2827303cde10cee1102999f27
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-08-15 21:14:19 -0400

    Combination sum backtracking progress

--- backtracking/39_combination_sum.py
+++ backtracking/39_combination_sum.py
@@ -35,4 +35,18 @@ from typing import List
 
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        pass
+        res = []
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i>=len(nums) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0,[], 0)
+        return res
