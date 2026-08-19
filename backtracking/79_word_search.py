0a5fdef84f82fe43ab999b8fb13e0e5b7c2f6c08
Ryan Shabaneh <ryan.shabaneh19@gmail.com>
2026-08-18 22:42:19 -0400

    solved 79 word search

--- backtracking/79_word_search.py
+++ backtracking/79_word_search.py
@@ -39,4 +39,34 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        pass
+        rows, cols = len(board), len(board[0])
+        chars = len(word)
+        visited = set()
+
+        def dfs(r,c,i):
+            if (i == chars):
+                return True
+            if (r < 0 or c < 0 or r >= rows or c >= cols):
+                return False
+            if (board[r][c]!= word[i] or (r,c) in visited):
+                return False
+ 
+            visited.add((r,c))
+            result = (dfs(r + 1, c, i + 1) or
+                      dfs(r - 1, c, i + 1) or
+                      dfs(r, c + 1, i + 1) or
+                      dfs(r, c - 1, i + 1))
+            visited.remove((r,c))
+            return result
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r,c,0):
+                    return True
+        
+        return False
+
+'''
+unmarks the whole route the entire explored path before the outer call moves onto the next cell
+so everything stays discoverable
+'''
